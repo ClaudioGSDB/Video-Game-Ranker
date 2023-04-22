f324ba4130a9954b282df6ec622b008974df4034
Claudio Sciotto <fisizion@gmail.com>
2023-04-22 16:54:45 -0400

    small changes

--- DataCollector/src/Main.java
+++ DataCollector/src/Main.java
@@ -17,8 +17,8 @@ public class Main
 
         FileWriter DataFile = new FileWriter("src/Data/DataTEST.txt");
 
-        for(int i = 0; i < 474; i++){
-        //for(int i = 0; i < 1; i++){ TEST
+        //for(int i = 0; i < 474; i++){
+        for(int i = 0; i < 1; i++){ //TEST
 
             String query = "fields name, rating, genres.name; where id > " + id + " & genres != null & rating != null; sort id asc; limit 500;";
 
