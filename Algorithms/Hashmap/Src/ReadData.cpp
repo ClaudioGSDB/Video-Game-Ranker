7f940b43d55cecad6bb638d2c97f98b9bba816e3
Claudio Sciotto <fisizion@gmail.com>
2023-04-23 15:59:12 -0400

    created hashmap files

---
+++ Algorithms/Hashmap/Src/ReadData.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Claudio on 4/23/2023.
+//
+
+#include "ReadData.h"
---
+++ Algorithms/Hashmap/Src/ReadData.h
@@ -0,0 +1,8 @@
+//
+// Created by Claudio on 4/23/2023.
+//
+
+#ifndef HASHMAP_READDATA_H
+#define HASHMAP_READDATA_H
+
+#endif //HASHMAP_READDATA_H
