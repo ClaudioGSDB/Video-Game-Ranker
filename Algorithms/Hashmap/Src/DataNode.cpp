2c0d7b1026656a3014bdbbc95acb53babf36d536
Claudio Sciotto <fisizion@gmail.com>
2023-04-23 17:43:34 -0400

    created readFile for hashmap

--- Algorithms/Hashmap/Src/DataNode.cpp
+++ Algorithms/Hashmap/Src/DataNode.cpp
@@ -1,25 +1,30 @@
 #include "DataNode.h"
 #include "ReadData.h"
 
-DataNode::DataNode(int ID, string title, float rating, map<int, string> genres){
+DataNode::DataNode(int ID, string title, float rating, map<int, string> genres)
+{
     this->ID = ID;
     this->title = title;
     this->rating = rating;
     this->genres = genres;
 }
 
-int DataNode::getId() const {
+int DataNode::getId() const
+{
     return ID;
 }
 
-const string &DataNode::getTitle() const {
+const string &DataNode::getTitle() const
+{
     return title;
 }
 
-float DataNode::getRating() const {
+float DataNode::getRating() const
+{
     return rating;
 }
 
-const map<int, string> &DataNode::getGenres() const {
+const map<int, string> &DataNode::getGenres() const
+{
     return genres;
 }
