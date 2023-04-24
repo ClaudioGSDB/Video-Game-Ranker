b41ecfe7bcc51fd9d2d134fe9be0fb788081cf57
torresja2911 <108757440+torresja2911@users.noreply.github.com>
2023-04-24 17:47:38 -0400

    genre data on graph folder

---
+++ Algorithms/Graph/cmake-build-debug/CMakeFiles/Graph.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graph.dir/Src/DataNode.cpp.obj"
+  "CMakeFiles/Graph.dir/Src/DataNode.cpp.obj.d"
+  "CMakeFiles/Graph.dir/Src/ReadData.cpp.obj"
+  "CMakeFiles/Graph.dir/Src/ReadData.cpp.obj.d"
+  "CMakeFiles/Graph.dir/Src/main.cpp.obj"
+  "CMakeFiles/Graph.dir/Src/main.cpp.obj.d"
+  "Graph.exe"
+  "Graph.exe.manifest"
+  "Graph.pdb"
+  "libGraph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- FilterData/main.cpp
+++ FilterData/main.cpp
@@ -2,7 +2,7 @@
 #include "dataSort.h"
 
 int main() {
-	dataSort cat("data/input/Data.txt", "../../Algorithms/Hashmap/cmake-build-debug/data/");
+	dataSort cat("data/input/Data.txt", "../../Algorithms/Graph/cmake-build-debug/data/");
 
 	cout << cat.gamesVisited.size() << endl;
 
